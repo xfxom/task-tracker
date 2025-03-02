8b673a34dfb7f772b8bd89db148f5d767d69231b
xfxom <xfnet12@gmail.com>
2025-03-02 13:36:43 +0300

    add query for found user tasks

--- src/main/java/com/track/task/repository/TaskRepository.java
+++ src/main/java/com/track/task/repository/TaskRepository.java
@@ -2,11 +2,18 @@ package com.track.task.repository;
 
 import com.track.task.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query("SELECT t FROM Task t WHERE t.user.email = :email")
+    List<Task> findAllByByUserEmail(String email);
+
     List<Task> findByVisibility(Boolean visible);
+
+    @Query("SELECT t FROM Task t WHERE t.user.email = :email AND t.visibility = :visible")
+    List<Task> findAllByUserEmailAndVisibility(String email, Boolean visible);
 }
